7d50c7cfd27cba6ef97311b2fb249961f5e7140b
Mukesh Kumar <thappamkkumar@gmail.com>
2026-05-26 12:42:32 +0530

    recreate project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Header from "@/components/layout/header/Header";
-import Hero from "@/components/sections/hero/Hero"; 
-import TrustedTechnologies from "@/components/sections/technologies/TrustedTechnologies"; 
+ 
 
 export default function Home() {
   return (
-    <main className="h-[2000px]">
-      <Header /> 
-      <Hero />
-      <TrustedTechnologies />
+    <main className="overflow-hidden h-[2000px]">
+      
     </main>
     
   );
